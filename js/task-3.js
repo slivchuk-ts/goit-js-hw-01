5e8175852c6b05926e7a7e94053806af96695a78
slivchuk-ts <taniaslivchuk97@gmail.com>
2024-05-17 16:58:34 +0200

    add homework

---
+++ js/task-3.js
@@ -0,0 +1,14 @@
+function getElementWidth(content, padding, border) {
+    content = parseFloat(content);
+    padding = parseFloat(padding);
+    border = parseFloat(border);
+    const width = content + (padding * 2) + (border * 2);
+    return width;
+    
+}
+
+console.log("-----task 3-----")
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log("")
